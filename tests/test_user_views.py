36f04c97d8d6934e12a9da3df803862204d6441a
pixls <tha.pixls@gmail.com>
2022-04-12 19:14:51 -0400

    starter user views tests

--- tests/test_user_views.py
+++ tests/test_user_views.py
@@ -20,27 +20,19 @@ def test_login(persisted_user):
 
         # Make sure it redirects
         assert resp.status_code == HTTPStatus.FOUND
-        # assert b"Hello, test_user!" in resp.data
 
 
-# def test_login(persisted_user):
-#     """
-#     GIVEN a valid user
-#     WHEN login is attempted
-#     THEN user is routed appropriately
-#     """
+def test_logout(persisted_user):
+    """
+    GIVEN a user already logged in
+    WHEN logout is attempted
+    THEN is user successfully removed from the session
 
-#     with app.test_client() as test_client:
-#         with test_client.session_transaction() as test_session:
-#             test_session[CURR_USER_KEY] = persisted_user.id
+    right now this test does not actually test anything other than the fact that '/logout' results in a redirect
+    """
 
-#         # Now, that session setting is saved, so we can have
-#         # the rest of ours test
-
-#         resp = test_client.post("/login", data={"text": "Hello"})
-
-#         # Make sure it redirects
-#         assert resp.status_code == HTTPStatus.FOUND
-
-#         msg = Message.query.one()
-#         assert msg.text == "Hello"
+    with app.test_client() as test_client:
+        
+        resp = test_client.get("/logout")
+        # Make sure it redirects
+        assert resp.status_code == HTTPStatus.FOUND
